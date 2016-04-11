190e8626c4a06b281e1e0db6783d7ed7e5e1ae52
Michael Lam <lamm@eecs.oregonstate.edu>
2016-04-11 10:49:29 -0700

    Add custom MDP

--- assignment2/main.py
+++ assignment2/main.py
@@ -1,14 +1,27 @@
 from mdp import MDP
 from mdp_optimization import MDPOptimization
 
+def print_value_function(a):
+    print '\n'.join('\t'.join('{0:0.4f}'.format(float(cell)) for cell in row) for row in a)
+
+def print_policy(a):
+    print '\n'.join('\t'.join('{0}'.format(int(cell)) for cell in row) for row in a)
+
 def print_helper(V, policy, name):
+    print "============================================================"
+    print
     print "=== {} ===".format(name)
     print "non-stationary value function:"
-    print V
+    print_value_function(V)
     print
     print "policy:"
-    print policy
+    print_policy(policy)
     print
+    print "============================================================"
+    print
+    print
+
+# PROBLEM 1
 
 # load MDP debug
 mdp = MDP()
@@ -18,6 +31,19 @@ mdp.load_from_file('MDP_debug.txt')
 H = 10
 (V, policy) = MDPOptimization.finite_horizon_value_iteration(mdp, H)
 print_helper(V, policy, "MDP Debug")
+
+# PROBLEM 2
+
+# load custom MDP
+mdp = MDP()
+mdp.load_from_file('MDP_custom.txt')
+
+# run finite horizon value iteration
+H = 10
+(V, policy) = MDPOptimization.finite_horizon_value_iteration(mdp, H)
+print_helper(V, policy, "MDP Custom")
+
+# PROBLEM 3
 
 # load MDP1
 mdp.load_from_file('MDP1.txt')
