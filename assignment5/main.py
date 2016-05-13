1550c07cb18f72f432ecf01c65ddacfaa89bcf9a
Michael Lam <lamm@eecs.oregonstate.edu>
2016-05-12 17:53:23 -0700

    Progress

--- assignment5/main.py
+++ assignment5/main.py
@@ -18,10 +18,37 @@ def print_state_helper(state):
         state_string = "({}, {}, {}, {})".format(a_or_b, row, occupied, parked)
         return state_string
 
-### PART II
-
-# create MDP
+### PART II: MDP 1
 mdp = ParkingMDP(10, handicap_reward=-100, collision_reward=-10000)
+
+# create simulator
+initial_state = 0
+simulator = MDPSimulator(mdp, initial_state=initial_state)
+
+# run simulation 1: random policy
+policy = RandomParkingPolicy(mdp, park_probability=0.1)
+avg_reward = 0
+num_trials = 10000
+for i in range(num_trials):
+    simulator.reset(initial_state=initial_state)
+    (total_reward, state_seq, action_seq) = simulator.run_simulation(policy)
+    avg_reward += total_reward
+avg_reward = 1.*avg_reward/num_trials
+print avg_reward
+
+# run simulation 2: safer random policy
+policy = SafeRandomParkingPolicy(mdp, park_probability=0.1)
+avg_reward = 0
+num_trials = 10000
+for i in range(num_trials):
+    simulator.reset(initial_state=initial_state)
+    (total_reward, state_seq, action_seq) = simulator.run_simulation(policy)
+    avg_reward += total_reward
+avg_reward = 1.*avg_reward/num_trials
+print avg_reward
+
+### PART II: MDP 2
+mdp = ParkingMDP(10, handicap_reward=100)
 
 # create simulator
 initial_state = 0
