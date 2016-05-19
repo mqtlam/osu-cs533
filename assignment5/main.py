4bcf1ca240268ebfd0820048976e816ef489e9fd
Michael Lam <lamm@eecs.oregonstate.edu>
2016-05-19 12:12:21 -0700

    Add Q-learner

--- assignment5/main.py
+++ assignment5/main.py
@@ -4,6 +4,7 @@ from mdp import MDP
 from parking_mdp import ParkingMDP, ParkingAction
 from policy import RandomParkingPolicy, SafeRandomParkingPolicy, SafeNoHandicapRandomParkingPolicy
 from simulator import MDPSimulator
+from qlearner import QLearner
 
 ### helper functions to print stuff
 
@@ -18,85 +19,108 @@ def print_state_helper(state):
         state_string = "({}, {}, {}, {})".format(a_or_b, row, occupied, parked)
         return state_string
 
+
 ### PART II: MDP 1
 num_rows = 10
-mdp = ParkingMDP(num_rows, handicap_reward=-100)
+mdp1 = ParkingMDP(num_rows, handicap_reward=-100)
 
 # create simulator
-initial_state = mdp.get_state_id(0, num_rows-1, 0, 0)
-simulator = MDPSimulator(mdp, initial_state=initial_state)
+initial_state1 = mdp1.get_state_id(0, num_rows-1, 0, 0)
+simulator = MDPSimulator(mdp1, initial_state=initial_state1)
 
 # run simulation 1: random policy
-policy = RandomParkingPolicy(mdp, park_probability=0.1)
+policy = RandomParkingPolicy(mdp1, park_probability=0.1)
 avg_reward = 0
 num_trials = 10000
 for i in range(num_trials):
-    simulator.reset(initial_state=initial_state)
+    simulator.reset(initial_state=initial_state1)
     (total_reward, state_seq, action_seq) = simulator.run_simulation(policy)
     avg_reward += total_reward
 avg_reward = 1.*avg_reward/num_trials
 print avg_reward
 
 # run simulation 2: safer random policy
-policy = SafeRandomParkingPolicy(mdp, park_probability=0.1)
+policy = SafeRandomParkingPolicy(mdp1, park_probability=0.1)
 avg_reward = 0
 num_trials = 10000
 for i in range(num_trials):
-    simulator.reset(initial_state=initial_state)
+    simulator.reset(initial_state=initial_state1)
     (total_reward, state_seq, action_seq) = simulator.run_simulation(policy)
     avg_reward += total_reward
 avg_reward = 1.*avg_reward/num_trials
 print avg_reward
 
 # run simulation 3: safer no handicap random policy
-policy = SafeNoHandicapRandomParkingPolicy(mdp, park_probability=0.1)
+policy = SafeNoHandicapRandomParkingPolicy(mdp1, park_probability=0.1)
 avg_reward = 0
 num_trials = 10000
 for i in range(num_trials):
-    simulator.reset(initial_state=initial_state)
+    simulator.reset(initial_state=initial_state1)
     (total_reward, state_seq, action_seq) = simulator.run_simulation(policy)
     avg_reward += total_reward
 avg_reward = 1.*avg_reward/num_trials
 print avg_reward
+
 
 ### PART II: MDP 2
-mdp = ParkingMDP(num_rows, handicap_reward=100)
+mdp2 = ParkingMDP(num_rows, handicap_reward=100)
 
 # create simulator
-initial_state = mdp.get_state_id(0, num_rows-1, 0, 0)
-simulator = MDPSimulator(mdp, initial_state=initial_state)
+initial_state2 = mdp2.get_state_id(0, num_rows-1, 0, 0)
+simulator = MDPSimulator(mdp2, initial_state=initial_state2)
 
 # run simulation 1: random policy
-policy = RandomParkingPolicy(mdp, park_probability=0.1)
+policy = RandomParkingPolicy(mdp2, park_probability=0.1)
 avg_reward = 0
 num_trials = 10000
 for i in range(num_trials):
-    simulator.reset(initial_state=initial_state)
+    simulator.reset(initial_state=initial_state2)
     (total_reward, state_seq, action_seq) = simulator.run_simulation(policy)
     avg_reward += total_reward
 avg_reward = 1.*avg_reward/num_trials
 print avg_reward
 
 # run simulation 2: safer random policy
-policy = SafeRandomParkingPolicy(mdp, park_probability=0.1)
+policy = SafeRandomParkingPolicy(mdp2, park_probability=0.1)
 avg_reward = 0
 num_trials = 10000
 for i in range(num_trials):
-    simulator.reset(initial_state=initial_state)
+    simulator.reset(initial_state=initial_state2)
     (total_reward, state_seq, action_seq) = simulator.run_simulation(policy)
     avg_reward += total_reward
 avg_reward = 1.*avg_reward/num_trials
 print avg_reward
 
 # run simulation 3: safer no handicap random policy
-policy = SafeNoHandicapRandomParkingPolicy(mdp, park_probability=0.1)
+policy = SafeNoHandicapRandomParkingPolicy(mdp2, park_probability=0.1, handicap_probability=1.0)
 avg_reward = 0
 num_trials = 10000
 for i in range(num_trials):
-    simulator.reset(initial_state=initial_state)
+    simulator.reset(initial_state=initial_state2)
     (total_reward, state_seq, action_seq) = simulator.run_simulation(policy)
     avg_reward += total_reward
 avg_reward = 1.*avg_reward/num_trials
 print avg_reward
 
+
 ### PART III
+policy = RandomParkingPolicy(mdp1, park_probability=0.1)
+qlearner = QLearner(mdp1, initial_state1)
+
+num_learning_trials = 10000
+num_simulation_trials = 1000
+num_learning_epochs = 5
+for epoch in range(num_learning_epochs):
+    print "Running learning {}...".format(epoch)
+    # run learning trials
+    for trial in range(num_learning_trials):
+        qlearner.run_learning_trial()
+
+    # run simulation trials
+    print "Running simulation {}...".format(epoch)
+    avg_reward = 0
+    for trial in range(num_simulation_trials):
+        (total_reward, state_seq, action_seq) = qlearner.run_simulation_trial()
+        avg_reward += total_reward
+    avg_reward = 1.*avg_reward/num_simulation_trials
+    print avg_reward
